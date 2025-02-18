916dc12cc54369093beaa6f8882d8b90a3602dc3
heesuy <bando2013@icloud.com>
2025-02-18 19:19:43 +0900

    Initial commit

---
+++ capstone_2025/capstone_2025/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  capstone_2025
+//
+//  Created by ㅇㅇ ㅇ on 2/18/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ capstone_2025/capstone_2025/capstone_2025App.swift
@@ -0,0 +1,17 @@
+//
+//  capstone_2025App.swift
+//  capstone_2025
+//
+//  Created by ㅇㅇ ㅇ on 2/18/25.
+//
+
+import SwiftUI
+
+@main
+struct capstone_2025App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
