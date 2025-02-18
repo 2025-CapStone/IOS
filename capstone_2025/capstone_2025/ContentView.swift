c6b1cc668c69198e29c834fcfdf5adfdb5b74e8e
heesuy <bando2013@icloud.com>
2025-02-18 19:40:00 +0900

    Add files via upload

---
+++ capstone_2025/capstone_2025/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  capstone_2025
+//
+//  Created by Yoon on 2/18/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("hi!!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ capstone_2025/capstone_2025/capstone_2025App.swift
@@ -0,0 +1,17 @@
+//
+//  capstone_2025App.swift
+//  capstone_2025
+//
+//  Created by Yoon on 2/18/25.
+//
+
+import SwiftUI
+
+@main
+struct capstone_2025App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
