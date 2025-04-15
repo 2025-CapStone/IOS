447ea3e4a7d051a3daf05580d066930aa9d18802
heesuy <bando2013@icloud.com>
2025-04-15 11:36:28 +0900

    login fix
    Remove token on App init

--- capstone_2025/capstone_2025/Preview Content/Presentation/capstone_2025App.swift
+++ capstone_2025/capstone_2025/Preview Content/Presentation/capstone_2025App.swift
@@ -5,11 +5,30 @@
 //  Created by Yoon on 2/18/25.
 //
 
+//import SwiftUI
+//
+//@main
+//struct capstone_2025App: App {
+//
+//    
+//    var body: some Scene {
+//        WindowGroup {
+//            ContentView()
+//        }
+//    }
+//}
 import SwiftUI
 
 @main
 struct capstone_2025App: App {
-    
+
+    init() {
+        // ✅ 앱 시작 시 토큰 초기화 (디버깅/테스트 목적)
+        UserDefaults.standard.removeObject(forKey: "accessToken")
+        UserDefaults.standard.removeObject(forKey: "refreshToken")
+        print("🧼 초기화: accessToken, refreshToken 제거 완료")
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
